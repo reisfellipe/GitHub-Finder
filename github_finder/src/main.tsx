c0751998ebc4e3c13c2a94b957f41f39b8a6c108
Fellipe Reis <reisfellipe11@gmail.com>
2025-01-18 20:20:22 -0300

    first part

---
+++ github_finder/src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//Pages
+import Home from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<App/>,
+    children:[
+      {
+        path: '/',
+        element:<Home/>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
