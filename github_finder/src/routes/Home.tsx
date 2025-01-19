3133f37c45ced7f5577ca89c797f73eb074ff0ff
Fellipe Reis <reisfellipe11@gmail.com>
2025-01-18 21:10:55 -0300

    creating dataUser

--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import Search from '../components/Search';
+import { UserProps } from '../types/user';
+import { useState } from 'react';
 
 const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async( userName:string ) => {
+
+        const res = await fetch(`https://api.github.com/users/${userName}`);
+        const data = await res.json();
+        const {avatar_url, login, location, followers, following} = data;
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following,
+        };
+
+    }
+
     return (
-        <div>Home</div>
+        <div>
+            <Search loadUser={loadUser}/>
+        </div>
     )
 }
 
